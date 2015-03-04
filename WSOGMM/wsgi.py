4a63fbf728aac332230fd62396b267cfc7c50e7a
Michael Filbin <michael.filbin@gmail.com>
2015-03-04 14:42:03 -0700

    Configure Django application to run in Heroku

--- WSOGMM/wsgi.py
+++ WSOGMM/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WSOGMM.settings")
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WSOGMM.settings")
 application = get_wsgi_application()
+
+if os.environ['ENVIRONMENT'] is 'Production':
+    application = Cling(application)
