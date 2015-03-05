c1f8da17bc0abca0ef3b54c50cf1df3c4fb8b0ac
Michael Filbin <michael.filbin@gmail.com>
2015-03-04 17:06:56 -0700

    Add initial data models with foreign key references

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField()
+    owner = models.ForeignKey(User)
+    # Parent references my parent group, if I have one
+    parent = models.ForeignKey("self", null=True, blank=True)
+    access_code = models.CharField()
+    expiration_date = models.DateTimeField()
+    created_by = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now=True)
+    modified_by = models.ForeignKey(User, null=True)
+    modified_at = models.DateTimeField()
+
+
+class Enrollment(models.Model):
+    group = models.ForeignKey(Group)
+    user = models.ForeignKey(User)
---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
