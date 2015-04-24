b2d534e28318f720d77c75480192f7122f66d787
Rob Newkirk <rob.newkirk@me.com>
2015-04-24 15:45:42 -0600

    At least it's valid Python now

--- dade/models.py
+++ dade/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-class School(models.Model):
-	name = models.CharField(max_length=128)
-	accounts = models.ManyToManyField(Account, verbose_name="Accounts")
-	groups = models.ManyToManyField(Group, verbose_name="Groups")
-	def __str__(self):
-		return self.name
-
 class Account(models.Model):
 	# id is done automagically
 	username = models.CharField(max_length=128)
@@ -16,23 +9,26 @@ class Account(models.Model):
 	priviledge = models.IntegerField(default=1)
 	is_confirmed = models.BooleanField(default=False)
 	confirmation_code = models.CharField(max_length=128)
-	school = models.ForeignKey(School)
+	school = models.ForeignKey("School")
 	def __str__(self):
 		return self.username
 
-class Group
+class Group(models.Model):
 	name = models.CharField(max_length=128)
-	school = models.ForeignKey(School)
+	school = models.ForeignKey("School")
 	accounts = models.ManyToManyField(Account, verbose_name="Accounts")
 
-class Assignment
+class School(models.Model):
+	name = models.CharField(max_length=128)
+	accounts = models.ManyToManyField(Account, verbose_name="Accounts")
+	groups = models.ManyToManyField(Group, verbose_name="Groups")
+	def __str__(self):
+		return self.name
+
+class Assignment(models.Model):
 	name = models.CharField(max_length=128)
 	school = models.ForeignKey(School)
 	group = models.ForeignKey(Group)
 
-class Notification
-	notifytype = models.CharField(max, length=128)
-
-
-class Schedule
-	
+class Notification(models.Model):
+	notifytype = models.CharField(max, max_length=128)
