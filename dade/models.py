bf0ba002dcdff7820714cba92ab57ac9bc0954af
Rob Newkirk <rob.newkirk@me.com>
2015-04-24 15:37:29 -0600

    Update models.py and remove *.pyc

--- dade/models.py
+++ dade/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class School(models.Model):
 	name = models.CharField(max_length=128)
-
-def __str__(self):
-	return self.name
+	accounts = models.ManyToManyField(Account, verbose_name="Accounts")
+	groups = models.ManyToManyField(Group, verbose_name="Groups")
+	def __str__(self):
+		return self.name
 
 class Account(models.Model):
-# id is done automagically
+	# id is done automagically
 	username = models.CharField(max_length=128)
 	password = models.CharField(max_length=256)
 	email = models.CharField(max_length=256)
@@ -17,4 +18,21 @@ class Account(models.Model):
 	confirmation_code = models.CharField(max_length=128)
 	school = models.ForeignKey(School)
 	def __str__(self):
-		return self.username 
+		return self.username
+
+class Group
+	name = models.CharField(max_length=128)
+	school = models.ForeignKey(School)
+	accounts = models.ManyToManyField(Account, verbose_name="Accounts")
+
+class Assignment
+	name = models.CharField(max_length=128)
+	school = models.ForeignKey(School)
+	group = models.ForeignKey(Group)
+
+class Notification
+	notifytype = models.CharField(max, length=128)
+
+
+class Schedule
+	
