444cc3f8061a8fcd242a9fb723ac078699da0e12
Michael Filbin <michael.filbin@gmail.com>
2015-05-08 16:35:38 -0600

    Add navigation bar

--- dade/views.py
+++ dade/views.py
@@ -5,18 +5,22 @@ from django.contrib import auth
 
 # Create your views here.
 def login(request):
-	if request.method == 'GET':
-		return render(request, 'base_login.html')
-	else:
-		email = request.POST.get('email','')
-		password = request.POST.get('password','')
-		user = auth.authenticate(username=email, password=password)
-		if user is not None and user.is_active:
-			return HttpResponseRedirect("/site/base_loggedin")
-		return render(request, 'base_login.html')
+    if request.method == 'GET':
+        return render(request, 'base_login.html')
+    else:
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+
+        user = auth.authenticate(username=email, password=password)
+
+        if user is not None and user.is_active:
+            return HttpResponseRedirect("/site/base_loggedin")
+        return render(request, 'base_login.html')
+
 
 def register(request):
-	return render(request,'base_register.html')
+    return render(request, 'base_register.html')
+
 
 def error(request):
-	return render(request, 'base_error.html')
+    return render(request, 'base_error.html')
