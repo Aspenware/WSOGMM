1a592949ed908555cc12aaf90dfdcd216de05bfb
Rob Newkirk <rob.newkirk@me.com>
2015-05-15 16:26:05 -0600

    Default routes

--- dade/urls.py
+++ dade/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 	url(r'^login/', views.login, name='login'),
 	url(r'^register/', views.register, name='register'),
 	url(r'^error/', views.error, name='error'),
+	url(r'', views.index, name='index')
 )
