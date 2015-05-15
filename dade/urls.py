84239c80253ce3caabf7c5a71c66095fb3caa1a8
Michael Filbin <michael.filbin@gmail.com>
2015-05-15 16:53:14 -0600

    Comment out index view route since it does not exist

--- dade/urls.py
+++ dade/urls.py
@@ -2,16 +2,9 @@ from django.conf.urls import patterns, url
 from dade import views
 
 urlpatterns = patterns('',
-<<<<<<< HEAD
                        url(r'^login/', views.login, name='login'),
                        url(r'^logout/', views.logout, name='logout'),
                        url(r'^register/', views.register, name='register'),
                        url(r'^error/', views.error, name='error'),
+                       #url(r'', views.index, name='index')
                        )
-=======
-	url(r'^login/', views.login, name='login'),
-	url(r'^register/', views.register, name='register'),
-	url(r'^error/', views.error, name='error'),
-	url(r'', views.index, name='index')
-)
->>>>>>> e3a100076f4b52b7de631e7ae670f225b16c6186
--- dade/views.py
+++ dade/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 from django.contrib import auth
-<<<<<<< HEAD
 from django.views.generic import RedirectView
-=======
 from django.contrib.auth.models import User
->>>>>>> e3a100076f4b52b7de631e7ae670f225b16c6186
+
 
 # Create your views here.
 def login(request):
