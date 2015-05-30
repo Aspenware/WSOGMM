c662e2c8599e0065b73d3019cc4c7fbe0d90c14e
Heny Rogers <henrytrogers@gmail.com>
2015-05-30 17:00:16 -0600

    Fix login

--- dade/views.py
+++ dade/views.py
@@ -3,8 +3,8 @@ from django.contrib import auth
 from django.views.generic import RedirectView
 from django.contrib.auth.models import User
 
-
 # Create your views here.
+
 def login(request):
     if request.method == 'GET':
       if request.user.is_authenticated():
@@ -17,21 +17,22 @@ def login(request):
 
         userR = User.objects.get(email=email)
 
-        user = auth.authenticate(user=userR.username, password=password)
-
+        user = auth.authenticate(username=userR.username, password=password)
+        print request.user
         if user is not None and user.is_active:
+            auth.login(request, user)
             return RedirectView.as_view(url="/site/login")(request)
+        print request.user
         return render(request, 'base_login.html')
 
 def logout(request):
     if request.user is not None and request.user.is_authenticated():
         auth.logout(request)
-
     return RedirectView.as_view(url='/site/login')(request)
 
 def register(request):
     return render(request, 'base_register.html')
-    
+
 def error(request):
     return render(request, 'base_error.html')
 
